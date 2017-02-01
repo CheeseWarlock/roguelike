1e00d39ccc5e6edb087b75630218a8d8faa08042
Joe Cardwell <joe.cardwell@gmail.com>
2017-02-01 20:29:46 +0000

    Add gulp-based build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
@@ -7,9 +10,25 @@ var paths = {
 	scripts: ['src/*.js']
 };
 
+gulp.task('libs', function() {
+	var b = browserify({
+		entries: 'src/main.js'
+	});
+
+	return b.bundle()
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(concat('libs.js'))
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('default', function() {
+	var b = browserify({
+		entries: 'src/main.js'
+	});
+
 	return gulp.src(paths.scripts)
-		.pipe(concat('all.js'))
+		.pipe(concat('game.js'))
 		.pipe(babel())
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
