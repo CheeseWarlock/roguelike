56b3443ac602c196b461204802e12515ee2a9f1c
Joe Cardwell <joe.cardwell@gmail.com>
2017-06-02 16:51:27 +0000

    Use classes for storing rooms and halls

--- src/DungeonLayout.js
+++ src/DungeonLayout.js
@@ -38,8 +38,12 @@ class EntitySpawn {
 
 class DungeonLayout {
 	constructor() {
-		this.rooms = [[0, 0, 3, 8, 0], [6, 3, 14, 14, 0], [0, 10, 4, 13, -50], [-6, -4, -3, 2, 100], [0, -10, 5, -1, 200]];
-		this.halls = [[3, 4, 6, 4, 0, 0], [4, 12, 6, 12, -50, 0], [1, 8, 1, 10, 0, -50], [-3, 1, 0, 1, 100, 0], [-3, -2, 0, -2, 100, 200]];
+		this.rooms = [
+			new Room(0, 0, 3, 8, 0), new Room(6, 3, 14, 14, 0), new Room(0, 10, 4, 13, -50), new Room(-6, -4, -3, 2, 100), new Room(0, -10, 5, -1, 200)
+		];
+		this.halls = [
+			new Hall(3, 4, 6, 4, 0, 0), new Hall(4, 12, 6, 12, -50, 0), new Hall(1, 8, 1, 10, 0, -50), new Hall(-3, 1, 0, 1, 100, 0), new Hall(-3, -2, 0, -2, 100, 200) 
+		];
 		this.doors = [[3, 4, 0], [6, 4, 0], [1, 8, 0], [1, 10, -50], [4, 12, -50], [6, 12, 0], [0, 1, 0], [-3, 1, 100], [-3, -2, 100], [0, -2, 200]];
 		this.entities = [[4, 4, 0, new Mandragora()], [1, 11, -50, new Bat()], [2, 11, -50, new Bat()]];
 	}
@@ -71,10 +75,10 @@ class DungeonLayout {
 
 	isInvisibleWall(x, z) {
 		for (const hallway of this.halls) {
-			if (hallway[0] == hallway[2]) {
-				if (z > hallway[1] && z < hallway[3] && (x == hallway[0] + 1 || x == hallway[0] - 1)) return true;
+			if (hallway.x1 == hallway.x2) {
+				if (z > hallway.z1 && z < hallway.z2 && (x == hallway.x1 + 1 || x == hallway.x1 - 1)) return true;
 			} else {
-				if (x > hallway[0] && x < hallway[2] && (z == hallway[1] + 1 || z == hallway[1] - 1)) return true;
+				if (x > hallway.x1 && x < hallway.x2 && (z == hallway.z1 + 1 || z == hallway.z1 - 1)) return true;
 			}
 		}
 		return false;
@@ -82,10 +86,10 @@ class DungeonLayout {
 
 	isHall(x, z) {
 		for (const hallway of this.halls) {
-			if (hallway[0] == hallway[2]) {
-				if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) return true;
+			if (hallway.x1 == hallway.x2) {
+				if (z >= hallway.z1 && z <= hallway.z2 && x == hallway.x1) return true;
 			} else {
-				if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) return true;
+				if (x >= hallway.x1 && x <= hallway.x2 && z == hallway.z1) return true;
 			}
 		}
 		return false;
@@ -94,22 +98,20 @@ class DungeonLayout {
 	height(x, z) {
 		var height = null;
 		for (const room of this.rooms) {
-			if (x >= room[0] && x <= room[2] && z >= room[1] && z <= room[3]) {
-				return room[4];
+			if (x >= room.x1 && x <= room.x2 && z >= room.z1 && z <= room.z2) {
+				return room.h;
 			}
 		}
 		for (const hallway of this.halls) {
-			if (hallway[0] == hallway[2]) {
-				// x same: vertical hall
-				if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) {
-					const slope = (hallway[4] - hallway[5]) / ((hallway[3] - hallway[1]) - 1);
-					return hallway[4] + (z - hallway[1] - 1.5) * slope;
+			if (hallway.x1 == hallway.x2) {
+				if (z >= hallway.z1 && z <= hallway.z2 && x == hallway.x1) {
+					const slope = (hallway.h1 - hallway.h2) / ((hallway.z2 - hallway.z1) - 1);
+					return hallway.h1 + (z - hallway.z1 - 1.5) * slope;
 				}
 			} else {
-				// z same: horiz hall
-				if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) {
-					const slope = (hallway[4] - hallway[5]) / ((hallway[2] - hallway[0]) - 1);
-					return hallway[4] + (x - hallway[0] - 0.5) * -slope;
+				if (x >= hallway.x1 && x <= hallway.x2 && z == hallway.z1) {
+					const slope = (hallway.h1 - hallway.h2) / ((hallway.x2 - hallway.x1) - 1);
+					return hallway.h1 + (x - hallway.x1 - 0.5) * -slope;
 				}
 			}
 		}
@@ -117,13 +119,13 @@ class DungeonLayout {
 
 	getTilt(x, z) {
 		for (const hallway of this.halls) {
-			if (hallway[0] == hallway[2]) {
-				if (z > hallway[1] && z < hallway[3] && x == hallway[0]) {
-					return [0, (hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5))];
+			if (hallway.x1 == hallway.x2) {
+				if (z > hallway.z1 && z < hallway.z2 && x == hallway.x1) {
+					return [0, (hallway.h2 < hallway.h1 ? 0.5 : (hallway.h2 == hallway.h1 ? 0 : -0.5))];
 				}
 			} else {
-				if (x > hallway[0] && x < hallway[2] && z == hallway[1]) {
-					return [(hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5)), 0];
+				if (x > hallway.x1 && x < hallway.x2 && z == hallway.z1) {
+					return [(hallway.h2 < hallway.h1 ? 0.5 : (hallway.h2 == hallway.h1 ? 0 : -0.5)), 0];
 				}
 			}
 		}
@@ -132,7 +134,7 @@ class DungeonLayout {
 
 	isFloor(x, z) {
 		for (const room of this.rooms) {
-			if (x > room[0] && x < room[2] && z > room[1] && z < room[3]) {
+			if (x > room.x1 && x < room.x2 && z > room.z1 && z < room.z2) {
 				return true;
 			}
 		}
@@ -141,8 +143,8 @@ class DungeonLayout {
 
 	isWall(x, z) {
 		for (const room of this.rooms) {
-			if (((x == room[0] || x == room[2]) && z >= room[1] && z <= room[3]) ||
-					((z == room[1] || z == room[3]) && x >= room[0] && x <= room[2])) {
+			if (((x == room.x1 || x == room.x2) && z >= room.z1 && z <= room.z2) ||
+					((z == room.z1 || z == room.z2) && x >= room.x1 && x <= room.x2)) {
 				return true;
 			}
 		}
