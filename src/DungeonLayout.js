1bdf4987acdc9435d0e850e46341ce72e4c64ef8
Joe Cardwell <joe.cardwell@gmail.com>
2017-06-02 05:24:58 +0000

    Rewrite DungeonLayout checks

--- src/DungeonLayout.js
+++ src/DungeonLayout.js
@@ -35,99 +35,90 @@ class DungeonLayout {
 	}
 
 	isDoor(x, z) {
-		var ret = false;
-		this.doors.map((door) => {
-			if (door[0] == x && door[1] == z) ret = true;
-		});
-		return ret;
+		for (const door of this.doors) {
+			if (door[0] == x && door[1] == z) return true;
+		}
+		return false;
 	}
 
 	isInvisibleWall(x, z) {
-		var ret = false;
-		this.halls.map((hallway) => {
+		for (const hallway of this.halls) {
 			if (hallway[0] == hallway[2]) {
-				if (z > hallway[1] && z < hallway[3] && (x == hallway[0] + 1 || x == hallway[0] - 1)) ret = true;
+				if (z > hallway[1] && z < hallway[3] && (x == hallway[0] + 1 || x == hallway[0] - 1)) return true;
 			} else {
-				if (x > hallway[0] && x < hallway[2] && (z == hallway[1] + 1 || z == hallway[1] - 1)) ret = true;
+				if (x > hallway[0] && x < hallway[2] && (z == hallway[1] + 1 || z == hallway[1] - 1)) return true;
 			}
-		});
-		return ret;
+		}
+		return false;
 	}
 
 	isHall(x, z) {
-		var ret = false;
-		this.halls.map((hallway) => {
+		for (const hallway of this.halls) {
 			if (hallway[0] == hallway[2]) {
-				if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) ret = true;
+				if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) return true;
 			} else {
-				if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) ret = true;
+				if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) return true;
 			}
-		});
-		return ret;
+		}
+		return false;
 	}
 
 	height(x, z) {
 		var height = null;
-		this.rooms.map((room) => {
+		for (const room of this.rooms) {
 			if (x >= room[0] && x <= room[2] && z >= room[1] && z <= room[3]) {
-				height = room[4];
+				return room[4];
 			}
-		});
-		if (height === null) {
-			this.halls.map((hallway) => {
-				if (hallway[0] == hallway[2]) {
-					// x same: vertical hall
-					if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) {
-						const slope = (hallway[4] - hallway[5]) / ((hallway[3] - hallway[1]) - 1);
-						height = hallway[4] + (z - hallway[1] - 1.5) * slope;
-					}
-				} else {
-					// z same: horiz hall
-					if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) {
-						const slope = (hallway[4] - hallway[5]) / ((hallway[2] - hallway[0]) - 1);
-						height = hallway[4] + (x - hallway[0] - 0.5) * -slope;
-					}
-				}
-			});
 		}
-		return height;
+		for (const hallway of this.halls) {
+			if (hallway[0] == hallway[2]) {
+				// x same: vertical hall
+				if (z >= hallway[1] && z <= hallway[3] && x == hallway[0]) {
+					const slope = (hallway[4] - hallway[5]) / ((hallway[3] - hallway[1]) - 1);
+					return hallway[4] + (z - hallway[1] - 1.5) * slope;
+				}
+			} else {
+				// z same: horiz hall
+				if (x >= hallway[0] && x <= hallway[2] && z == hallway[1]) {
+					const slope = (hallway[4] - hallway[5]) / ((hallway[2] - hallway[0]) - 1);
+					return hallway[4] + (x - hallway[0] - 0.5) * -slope;
+				}
+			}
+		}
 	}
 
 	getTilt(x, z) {
-		var ret = [0, 0];
-		this.halls.map((hallway) => {
+		for (const hallway of this.halls) {
 			if (hallway[0] == hallway[2]) {
 				if (z > hallway[1] && z < hallway[3] && x == hallway[0]) {
-					ret = [0, (hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5))];
+					return [0, (hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5))];
 				}
 			} else {
 				if (x > hallway[0] && x < hallway[2] && z == hallway[1]) {
-					ret = [(hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5)), 0];
+					return [(hallway[5] < hallway[4] ? 0.5 : (hallway[5] == hallway[4] ? 0 : -0.5)), 0];
 				}
 			}
-		});
-		return ret;
+		}
+		return [0, 0];
 	}
 
 	isFloor(x, z) {
-		var floor = false;
-		this.rooms.map((room) => {
+		for (const room of this.rooms) {
 			if (x > room[0] && x < room[2] && z > room[1] && z < room[3]) {
-				floor = true;
+				return true;
 			}
-		});
-		return floor;
+		}
+		return false;
 	}
 
 	isWall(x, z) {
-		var wall = false;
-		this.rooms.map((room) => {
+		for (const room of this.rooms) {
 			if (((x == room[0] || x == room[2]) && z >= room[1] && z <= room[3]) ||
 					((z == room[1] || z == room[3]) && x >= room[0] && x <= room[2])) {
-				wall = true;
+				return true;
 			}
-		});
-		return wall;
+		}
+		return false;
 	}
 }
 
